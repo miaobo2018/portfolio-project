4bd53797e4ef277db4246c066e1bf060b9b1b3ba
Bo Miao <bo.miao@sjsu.edu>
2019-09-11 23:12:46 -0700

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,19 @@ from django.db import models
 # image
 class Blog(models.Model):
 	title = models.CharField(max_length=255)
-	pub_data = models.DateTimeField()
+	pub_date = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100] + '...'
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
 
 # Add the Blog app to the settings
 
