a926b2e8eff6dcfcceec328fa43e8012481ef066
BoMiao2020 <miaobo201808@gmail.com>
2020-06-30 17:43:12 -0500

    test

--- jobs/views.py
+++ jobs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import Job
 
+
 def home(request):
-	jobs = Job.objects # transform the database type to python type
-	return render(request, 'jobs/home.html', {'jobs':jobs})
+    jobs = Job.objects  # test transform the database type to python type
+    return render(request, 'jobs/home.html', {'jobs': jobs})
